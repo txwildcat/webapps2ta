a3a6a4f9ed76ccb756b81d7376710aab97e47d44
txwildcat <tyler.askren@kellogg.northwestern.edu>
2022-02-20 03:36:17 +0000

    got lost late

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,4 +4,10 @@ class ContactsController < ApplicationController
     @contact = Contact.find(params["id"])
   end
 
+  def new
+    @contact = Contact.new
+    @contact.company_id = params["company_id"]
+  end
+
+
 end
